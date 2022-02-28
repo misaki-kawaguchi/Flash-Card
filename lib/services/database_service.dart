b4f58e686415e411279178226f01548e45e05404
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-02-28 18:42:25 +0900

    データベースの初期化処理を追加

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,3 +1,8 @@
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
+
 // データベース管理用クラス（シングルトンとして作成して利用することによってどこでも共通の値を利用することができる）
 class DatabaseService {
 
@@ -13,4 +18,20 @@ class DatabaseService {
 
   // デフォルトのコンストラクタをプライベートアクセス
   DatabaseService._internal();
+
+  // late：デフォルト値を入れておかなくても null 許容でない値として扱うことができる
+  late Database _db;
+
+  // 初期化
+  Future initialize() async {
+    // アプリケーション実行時に読み書きできるフォルダの位置を取得する
+    final dir = await getApplicationDocumentsDirectory();
+    final path = join(dir.path, 'databse.db');
+
+    // データベースファクトリを使用してデータベースを開く
+    _db = await databaseFactoryIo.openDatabase(path);
+  }
+
+  // DatabaseService().dbのような形でDatabaseを参照して利用できるようになる
+  Database get db => _db;
 }
