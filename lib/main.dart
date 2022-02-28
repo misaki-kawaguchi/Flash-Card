dad8cfa2c73d141a8e4baa8725994f3f471784f5
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-02-28 15:27:25 +0900

    ルーティングを設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flashcard/pages/home.dart';
+import 'routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      routes: routes,
     );
   }
 }
