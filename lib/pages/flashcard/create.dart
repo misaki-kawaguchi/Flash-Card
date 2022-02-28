bd48807c3d2bf372e5f0fc1d0c41f3c993d68ea9
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-02-28 15:15:04 +0900

    pages/flashcard/create.dartを作成

---
+++ lib/pages/flashcard/create.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateFlashcardPage extends StatefulWidget {
+  const CreateFlashcardPage({Key? key}) : super(key: key);
+
+  @override
+  _CreateFlashcardPageState createState() => _CreateFlashcardPageState();
+}
+
+class _CreateFlashcardPageState extends State<CreateFlashcardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
