7c85598d1cc8e69d766adefca4e5e2fe017ef430
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-02-28 19:07:36 +0900

    データを保存するためのstoreを作成

---
+++ lib/repositories/flashcard_repository.dart
@@ -0,0 +1,10 @@
+import 'package:sembast/sembast.dart';
+
+class FlashcardRepository {
+
+  // キー値を文字列として保存するためのメインストアを設定
+  static StoreRef<int, Map<String, Object?>> store() {
+    // キーをint、値をMapとして格納する（flashcardsというデータストアを定義して利用 ）
+    return intMapStoreFactory.store('flashcards');
+  }
+}
