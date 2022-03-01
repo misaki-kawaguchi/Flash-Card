02f4b3b3bd20e2e4a4bbca06b08658901399174b
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-03-01 14:33:20 +0900

    編集ページを作成

---
+++ lib/pages/flashcard/edit.dart
@@ -0,0 +1,75 @@
+import 'package:flashcard/models/flashcard.dart';
+import 'package:flashcard/repositories/flashcard_repository.dart';
+import 'package:flashcard/widgets/flashcard/flashcard_form.dart';
+import 'package:flutter/material.dart';
+
+class EditFlashcardPage extends StatefulWidget {
+  const EditFlashcardPage({Key? key}) : super(key: key);
+
+  @override
+  _EditFlashcardPageState createState() => _EditFlashcardPageState();
+}
+
+class _EditFlashcardPageState extends State<EditFlashcardPage> {
+
+  // フォームと連携させるためのGlobalKey
+  final _formKey = GlobalKey<FormState>();
+
+  // 名前編集欄用のコントローラー（フォームの値を管理）
+  final _nameController = TextEditingController();
+
+  // 新規登録ボタンが押された時にバリデーションを働かせる
+  Future _save() async {
+    // フォーム内のバリデーションを実行する
+    // currentState!：nullではないことを表す
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    // Flashcardモデルのインスタンスを作成
+    final flashcard = Flashcard(name: _nameController.text);
+    // データを保存
+    await FlashcardRepository.add(flashcard);
+
+    // 前の画面に戻る
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('単語帳を編集'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  FlashcardForm(nameController: _nameController),
+                  Container(
+                    margin: const EdgeInsets.all(20.0),
+                    child: ElevatedButton(
+                      onPressed: _save,
+                      child: const Text('更新'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // TextEditingControllerはウィジェット上での利用が終わったあとはリソースを解放してあげる必要がある
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+}
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,14 @@
 import 'package:flashcard/pages/home.dart';
 import 'package:flashcard/pages/flashcard/create.dart';
+import 'package:flashcard/pages/flashcard/edit.dart';
 
 // 一番最初に開かれるページ
 const indexPage = '/';
 const flashcardCreatePage = '/flashcard/create';
+const flashcardEditPage = '/flashcard/edit';
 
 final routes = {
   indexPage: (context) => const MyHomePage(),
   flashcardCreatePage: (context) => const CreateFlashcardPage(),
+  flashcardEditPage: (context) => const EditFlashcardPage(),
 };
