80b42e71230b94f502addea477a73c2be448b546
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-03-01 14:13:14 +0900

    共通部分を追加

--- lib/widgets/flashcard/flashcard_form.dart
+++ lib/widgets/flashcard/flashcard_form.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
 class FlashcardForm extends StatelessWidget {
-  const FlashcardForm({Key? key}) : super(key: key);
+  const FlashcardForm({Key? key, required this.nameController})
+      : super(key: key);
+
+  final TextEditingController nameController;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextFormField(
+      controller: nameController,
+      decoration: const InputDecoration(
+        labelText: '名前',
+        hintText: '名前',
+      ),
+      validator: (String? value) {
+        if (value == null || value.isEmpty) {
+          return '入力してください';
+        }
+        // 入力値が正常な場合はnullを返す
+        return null;
+      },
+    );
   }
 }
