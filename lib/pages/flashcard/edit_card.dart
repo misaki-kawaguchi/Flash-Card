3204eb7cf89a4e5c3b1d04012cb9fabc8ffe71bd
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-03-07 16:44:52 +0900

    カード編集画面を作成

---
+++ lib/pages/flashcard/edit_card.dart
@@ -0,0 +1,60 @@
+import 'package:flashcard/models/flashcard_card.dart';
+import 'package:flashcard/widgets/flashcard/flashcard_card_form.dart';
+import 'package:flutter/material.dart';
+import 'package:flashcard/routes.dart';
+
+class EditFlashcardCardPage extends StatelessWidget {
+  const EditFlashcardCardPage({Key? key}) : super(key: key);
+
+  // 編集用のカードデータをargumentパラメータを指定してプロパティで受け取る
+  static Future push(BuildContext context, FlashcardCard flashcardCard) async {
+    return Navigator.of(context).pushNamed(flashcardCardEditPage, arguments: flashcardCard);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    FlashcardCard getFlashcardCard() {
+      // パラメータを読み込む
+      return ModalRoute.of(context)!.settings.arguments as FlashcardCard;
+    }
+
+    // 更新する
+    Future update(String question, String answer) async {}
+
+    // 削除する
+    Future delete() async {}
+
+    final flashcardCard = getFlashcardCard();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('カードを編集'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(20),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              FlashcardCardForm(
+                onSave: update,
+                flashcardCard: flashcardCard,
+                buttonText: '更新する',
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 80),
+                child: TextButton(
+                  onPressed: delete,
+                  style: TextButton.styleFrom(
+                    primary: Theme.of(context).errorColor,
+                  ),
+                  child: const Text('削除'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,14 +1,17 @@
 import 'package:flashcard/pages/home.dart';
 import 'package:flashcard/pages/flashcard/create.dart';
 import 'package:flashcard/pages/flashcard/edit.dart';
+import 'package:flashcard/pages/flashcard/edit_card.dart';
 
 // 一番最初に開かれるページ
 const indexPage = '/';
 const flashcardCreatePage = '/flashcard/create';
 const flashcardEditPage = '/flashcard/edit';
+const flashcardCardEditPage = '/flashcard/edit-card';
 
 final routes = {
   indexPage: (context) => const MyHomePage(),
   flashcardCreatePage: (context) => const CreateFlashcardPage(),
   flashcardEditPage: (context) => const EditFlashcardPage(),
+  flashcardCardEditPage: (context) => const EditFlashcardCardPage(),
 };
